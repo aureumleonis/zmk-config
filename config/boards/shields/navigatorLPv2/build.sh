b230d67c85610e35da67717c1685e57471ba76ff
aureumleonis <xbullethammer@gmail.com>
2025-03-12 10:21:03 -0700

    Temp change for nav4

--- config/boards/shields/navigatorLPv2/build.sh
+++ config/boards/shields/navigatorLPv2/build.sh
@@ -4,6 +4,5 @@ side=$1
 cd ~/tools/zmk/app/
 
 set -e
-west build -p -d build/${side}_lpv2 -b nice_nano_v2 -- -DSHIELD=navigatorLPv2_$side -DZMK_CONFIG="$HOME/projects/zmk-config/config"
-cp build/${side}_lpv2/zephyr/zmk.uf2 /mnt/c/Users/$USER/Documents/zmk_lpv2_${side}.uf2
-
+west build -p -d build/${side}_v4 -b nice_nano_v2 -- -DSHIELD=navigatorLPv2_$side -DZMK_CONFIG="$HOME/projects/zmk-config/config"
+cp build/${side}_v4/zephyr/zmk.uf2 /mnt/c/Users/$USER/Documents/zmk_v4_${side}.uf2
