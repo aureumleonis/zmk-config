86d19b7a663081c5fa57435537b32fc515645862
aureumleonis <xbullethammer@gmail.com>
2025-08-17 23:30:54 -0700

    Testing trackpoint

--- config/boards/shields/navigatorLPv2/build.sh
+++ config/boards/shields/navigatorLPv2/build.sh
@@ -4,5 +4,5 @@ side=$1
 cd ~/tools/zmk/app/
 
 set -e
-west build -p -d build/${side}_v4 -b nice_nano_v2 -- -DSHIELD=navigatorLPv2_$side -DZMK_CONFIG="$HOME/projects/zmk-config/config"
+west build -p -d build/${side}_v4 -b nice_nano_v2 -S zmk-usb-logging -- -DZMK_EXTRA_MODULES="$HOME/tools/kb_zmk_ps2_mouse_trackpoint_driver/" -DSHIELD=navigatorLPv2_$side -DZMK_CONFIG="$HOME/projects/zmk-config/config"
 cp build/${side}_v4/zephyr/zmk.uf2 /mnt/c/Users/$USER/Documents/zmk_v4_${side}.uf2
